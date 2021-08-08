9d6448a85938a42c54a283647e70a17340c91db6
fandi3 <fandi3@jd.com>
2021-08-08 18:16:58 +0800

    week01 leetcode189 commit

---
+++ src/main/week01/LeetCode189.java
@@ -0,0 +1,47 @@
+package week01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 旋转数组
+ * https://leetcode-cn.com/problems/rotate-array/
+ */
+public class LeetCode189 {
+
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        foo(nums, 3);
+        int[] nums1 = {-1, -100, 3, 99};
+        foo(nums1, 2);
+
+        for (int num : nums) {
+            System.out.print(num + "\t");
+        }
+        System.out.println();
+
+        for (int i : nums1) {
+            System.out.print(i + "\t");
+        }
+    }
+
+    private static void foo(int[] nums, int k) {
+        int length = nums.length;
+
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
+}
