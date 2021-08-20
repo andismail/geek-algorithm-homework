1794f5177d323f5273bc43e1d93fe12db3cc3bcd
fandi3 <fandi3@jd.com>
2021-08-20 15:19:28 +0800

    week02 repeat

---
+++ src/main/week01/Repeat5.java
@@ -0,0 +1,171 @@
+package week01;
+
+import common.ListNode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 26.删除排序数组中的重复项
+ * 189.旋转数组
+ * 21.合并两个有序链表
+ * 88.合并两个有序数组
+ * 1.两数之和
+ * 283.移动零
+ * 66.加一
+ */
+public class Repeat5 {
+
+    public int _26(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int p = 0, q = 1;
+        while (q < nums.length) {
+            if (nums[p] != nums[q]) {
+                if (q - p == 1) {
+                    p++;
+                    q++;
+                    continue;
+                }
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+
+    public void _189(int[] nums, int k) {
+        k %= nums.length;
+
+        revert(nums, 0, nums.length - 1);
+        revert(nums, 0, k - 1);
+        revert(nums, k, nums.length - 1);
+    }
+
+    private void revert(int[] nums, int s, int e) {
+        while (s < e) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+
+            s++;
+            e--;
+        }
+    }
+
+    public ListNode _21(ListNode n1, ListNode n2) {
+        if (n1 == null) {
+            return n2;
+        }
+        if (n2 == null) {
+            return n1;
+        }
+
+        if (n1.val < n2.val) {
+            n1.next = _21(n1.next, n2);
+            return n1;
+        }
+
+        n2.next = _21(n1, n2.next);
+        return n2;
+    }
+
+    public void _88_1(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+    }
+
+    public void _88_2(int[] nums1, int m, int[] nums2, int n) {
+        int[] sort = new int[m + n];
+        int curr;
+        int p1 = 0, p2 = 0;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                curr = nums2[p2++];
+            } else if (p2 == n) {
+                curr = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                curr = nums1[p1++];
+            } else {
+                curr = nums2[p2++];
+            }
+            sort[p1 + p2 - 1] = curr;
+        }
+        for (int i = 0; i < sort.length; i++) {
+            nums1[i] = sort[i];
+        }
+    }
+
+    public void _88_3(int[] nums1, int m, int[] nums2, int n) {
+        int tail = m + n - 1;
+        int curr;
+        int p1 = m - 1, p2 = n - 1;
+        while (p1 > -1 || p2 > -1) {
+            if (p1 == -1) {
+                curr = nums2[p2--];
+            } else if (p2 == -1) {
+                curr = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                curr = nums1[p1--];
+            } else {
+                curr = nums2[p2--];
+            }
+            nums1[tail--] = curr;
+        }
+    }
+
+    public int[] _01(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+
+    public void _283(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int p = 0, q = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[q] != 0) {
+                if (p == q) {
+                    p++;
+                    q++;
+
+                    continue;
+                }
+
+                nums[p] = nums[q];
+                nums[q] = 0;
+                p++;
+            }
+            q++;
+        }
+    }
+
+    public int[] _66(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            nums[i]++;
+            nums[i] %= 10;
+            if (nums[i] != 0) {
+                return nums;
+            }
+        }
+        nums = new int[nums.length + 1];
+        nums[0] = 1;
+        return nums;
+    }
+}
---
+++ src/main/week02/Reteat03.java
@@ -0,0 +1,133 @@
+package week02;
+
+import common.NTreeNode;
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 242.有效的字母异位词
+ * 49.异位词分组
+ * 94.二叉树的中序遍历
+ * 144.二叉树的前序遍历
+ * 589.N叉树的前序遍历
+ * 590.N叉树的后序遍历
+ * N叉树的层序遍历
+ */
+public class Reteat03 {
+    public boolean _242(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] charArray = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            charArray[s1.charAt(i) - 'a']++;
+            charArray[s2.charAt(i) - 'a']--;
+        }
+        for (int i : charArray) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<List<String>> _49(String[] strs) {
+        Map<Double, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            double key = calcKey(str);
+            List<String> strings = map.computeIfAbsent(key, v -> new ArrayList<>());
+            strings.add(str);
+        }
+        List<List<String>> res = new ArrayList<>();
+        for (Double key : map.keySet()) {
+            res.add(map.get(key));
+        }
+        return res;
+    }
+
+    private double calcKey(String str) {
+        int[] x = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+
+        double y = 1d;
+        for (int i = 0; i < str.length(); i++) {
+            y *= x[str.charAt(i) - 'a'];
+        }
+        return y;
+    }
+
+    public List<Integer> _94(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        foo(root, res);
+        return res;
+    }
+
+    private void foo(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        foo(node.left, res);
+        res.add(node.val);
+        foo(node.right, res);
+    }
+
+    public List<Integer> _144(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        foo1(root, res);
+        return res;
+    }
+
+    private void foo1(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        res.add(node.val);
+        foo1(node.left, res);
+        foo1(node.right, res);
+    }
+
+    public List<Integer> _589(NTreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        foo2(root, res);
+        return res;
+    }
+
+    private void foo2(NTreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        res.add(node.val);
+        if (node.children != null) {
+            for (NTreeNode childNode : node.children) {
+                foo2(childNode, res);
+            }
+        }
+    }
+
+    public List<Integer> _590(NTreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        foo3(root, res);
+        return res;
+    }
+
+    private void foo3(NTreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.children != null) {
+            for (NTreeNode childNode : node.children) {
+                foo2(childNode, res);
+            }
+        }
+        res.add(node.val);
+    }
+
+}
